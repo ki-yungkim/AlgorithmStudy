b910ab2581b67e96e53c0bcaf3a83782997ffc8a
ki-yungkim <kiyungdev@gmail.com>
2021-06-17 23:47:09 +0900

    Create 01_mock_test.py

---
+++ 02_bruteForce_binarySearch/01_mock_test/01_mock_test.py
@@ -0,0 +1,21 @@
+def solution(answers):
+    answer = []
+    score = [0, 0, 0]
+    a = [1,2,3,4,5] 
+    b = [2,1,2,3,2,4,2,5] 
+    c = [3,3,1,1,2,2,4,4,5,5] 
+    
+    for i in range(len(answers)):
+        if a[i%len(a)] == answers[i]:
+            score[0] += 1
+        if b[i%len(b)] == answers[i]:
+            score[1] += 1
+        if c[i%len(c)] == answers[i]:
+            score[2] += 1
+    
+    max_score = max(score)
+    for i in range(0, len(score)):
+        if score[i] == max(score):
+            answer.append(i+1)
+    
+    return answer
